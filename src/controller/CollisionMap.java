cd73b4ef89f19d17a19b0a9554b6c7dece135fbc
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-04-09 14:36:50 +0200

    Improve (a *lot*) collision detection performance

--- src/controller/CollisionMap.java
+++ src/controller/CollisionMap.java
@@ -8,12 +8,19 @@ import java.io.File;
 
 public class CollisionMap {
     private BufferedImage colImage;
+    private int[][] map;
 
     public CollisionMap(String collisionImagePath)
         throws LemmingsException {
         try {
             /* Note that colImage type is TYPE_4BYTE_ABGR */
             colImage = ImageIO.read(new File(collisionImagePath));
+            map = new int[getWidth()][getHeight()];
+            //colImage.getRGB(0, 0, getWidth(), getHeight(), map, 0, 0);
+            /* TODO: get the RGB of a region, which is faster */
+            for (int x = 0; x < getWidth(); x++)
+                for (int y = 0; y < getHeight(); y++)
+                    map[x][y] = (colImage.getRGB(x, y) >> 24);
         } catch (java.io.IOException e) {
             throw new LemmingsException("controller",
                                         "Can't load image '" +
@@ -31,13 +38,14 @@ public class CollisionMap {
     }
 
     public boolean isCollisionFree(int x, int y, int w, int h) {
-        /* TODO: is there something more efficient ? */
-        int[] argb = new int[x*y];
-        colImage.getRGB(x, y, w, h, argb, 0, 0);
-        for (int pixel : argb) {
-            int alpha = pixel >> 24;
-            if (alpha != 0) /* alpha seems to have 0 or -1 as value */
-                return false;
+        /* This is the bottleneck function */
+        int pixel, i, j;
+        for (i = 0; i < w; i++) {
+            for (j = 0; j < h; j++) {
+                if (map[x+i][y+j] != 0)
+                    /* alpha seems to have 0 or -1 as value */
+                    return false;
+            }
         }
         return true;
     }
