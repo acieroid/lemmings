72a24fa72cdf66f4268b16328a3b80c0a6196a0f
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-04-10 23:42:54 +0200

    Improving the menu

--- src/view/Menu.java
+++ src/view/Menu.java
@@ -23,6 +23,9 @@ public class Menu extends BasicGameState {
 
     private MapSelector maps;
 
+    private int itemSelected;
+    private String[] items = {"Play", "Map", "Quit"};
+
     public Menu(MapSelector maps) {
         this.maps = maps;
         this.font = font;
@@ -32,34 +35,57 @@ public class Menu extends BasicGameState {
         return ID;
     }
 
-    private void drawCenteredText(GameContainer gc, String text) {
-        font.drawString(gc.getWidth()/2 - font.getWidth(text)/2,
-                        gc.getHeight()/2 - font.getLineHeight()/2,
-                        text, Color.white);
+    private void drawCenteredText(GameContainer gc, String text,
+                                  int line, int nLines, Color color) {
+        int spacing = font.getLineHeight()-5;
+        int start = gc.getHeight()/2 - (spacing+font.getLineHeight())*nLines/2;
+        int x = gc.getWidth()/2 - font.getWidth(text)/2;
+        int y = start + line*(spacing+font.getLineHeight());
+        font.drawString(x, y, text, color);
     }
 
-    public void init(GameContainer c, StateBasedGame game)
+    public void init(GameContainer gc, StateBasedGame game)
         throws SlickException {
         this.game = game;
-        font = c.getGraphics().getFont();
+        font = gc.getGraphics().getFont();
     }
 
-    public void render(GameContainer c, StateBasedGame game, Graphics g) {
-        drawCenteredText(c, "< " + maps.current() + " >");
+    public void render(GameContainer gc, StateBasedGame game, Graphics g) {
+        String text;
+        for (int i = 0; i < items.length; i++) {
+            text = items[i];
+            if (text.equals("Map"))
+                text = "Map : < " + maps.current() + " >";
+            drawCenteredText(gc, text, i, items.length,
+                             (i == itemSelected) ? Color.green : Color.white);
+        }
     }
 
-    public void update(GameContainer c, StateBasedGame game, int delta) {
+    public void update(GameContainer gc, StateBasedGame game, int delta) {
+        Input input = gc.getInput();
+        if (input.isKeyDown(Input.KEY_ENTER) &&
+            items[itemSelected].equals("Quit"))
+            gc.exit();
     }
 
     /**
      * Handle a key press
      */
     public void keyPressed(int key, char c) {
-        if (key == Input.KEY_LEFT)
-            maps.previous();
-        else if (key == Input.KEY_RIGHT)
-            maps.next();
-        else if (key == Input.KEY_ENTER)
+        if (key == Input.KEY_DOWN)
+            itemSelected = Math.min(itemSelected+1, items.length-1);
+        else if (key == Input.KEY_UP)
+            itemSelected = Math.max(itemSelected-1, 0);
+
+        if (items[itemSelected].equals("Play") && key == Input.KEY_ENTER)
             game.enterState(View.ID);
+        else if (items[itemSelected].equals("Quit"))
+            ; /* handled in update */
+        else { /* Map */
+            if (key == Input.KEY_LEFT)
+                maps.previous();
+            else if (key == Input.KEY_RIGHT)
+                maps.next();
+        }
     }
 }
