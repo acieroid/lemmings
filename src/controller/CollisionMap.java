4a0d07e691b36de7fba3153ba5730c4effbf5b4a
Quentin Stievenart <quentin.stievenart@gmail.com>
2012-04-11 23:51:09 +0200

    Improving the second part of map loading

--- src/controller/CollisionMap.java
+++ src/controller/CollisionMap.java
@@ -25,18 +25,20 @@ public class CollisionMap {
 
             /* Load the entrance and exit position */
             BufferedImage objects = ImageIO.read(new File(directory + "/objects.png"));
+            int[] data = new int[getWidth()*getHeight()];
+            objects.getRGB(0, 0, getWidth(), getHeight(), data, 0, getWidth());
             boolean entranceFound = false, exitFound = false;
             for (int x = 0; x < getWidth(); x++) {
                 for (int y = 0; y < getHeight(); y++) {
                     if (!entranceFound &&
-                        (objects.getRGB(x, y) & 0xFF00) >> 8 == 0xFF) {
+                        (data[y*getWidth() + x] & 0xFF00) >> 8 == 0xFF) {
                         /* Green: the entrance */
                         entranceX = x;
                         entranceY = y;
                         entranceFound = true;
                     }
                     else if (!exitFound &&
-                             (objects.getRGB(x, y) & 0xFF0000) >> 16 == 0xFF) {
+                             (data[y*getWidth() + x] & 0xFF0000) >> 16 == 0xFF) {
                         /* Red: the exit */
                         exitX = x;
                         exitY = y;
